a5ac46bc97f531d6536da250d9b871ecc13754dd
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-11 15:09:49 -0400

    Adding solutions for theano tensor solutions

--- tensor.py
+++ tensor.py
@@ -1,53 +1,56 @@
 import numpy as np
 from theano import function
+import theano.tensor as T
 
 def make_tensor(dim):
     """
-    dim: The total number of dimensions of the tensor. 
     Returns a new Theano tensor with no broadcastable dimensions.
+    dim: the total number of dimensions of the tensor.
     """
 
-    raise NotImplementedError("TODO: implement this function.")
+    return T.TensorType(broadcastable=tuple([False] * dim), dtype='float32')()
 
 def broadcasted_add(a, b):
     """
-    a: 3D theano tensor
-    b: 4D theano tensor
-    Returns: C, a 4D theano tensor where:
-        c[i, j, k, l] = a[l, k, i] + b[i, j, k, l]
+    a: a 3D theano tensor
+    b: a 4D theano tensor
+    Returns c, a 4D theano tensor, where
 
-        for all i, j, k, l
+    c[i, j, k, l] = a[l, k, i] + b[i, j, k, l]
+
+    for all i, j, k, l
     """
 
-    raise NotImplementedError("TODO: implement this function.")
+    return a.dimshuffle(2, 'x', 1, 0) + b
 
 def partial_max(a):
     """
-    a: 4D theano tensor
+    a: a 4D theano tensor
 
-    Returns b, a theano matrix where:
-        b[i, j] = max_{k, l} a[i, j, k, l]
+    Returns b, a theano matrix, where
 
-        for all i, j
+    b[i, j] = max_{k,l} a[i, k, l, j]
+
+    for all i, j
     """
 
-    raise NotImplementedError("TODO: implement this function.")
+    return a.max(axis=(1, 2))
 
-    if __name__ == "__main__":
-        a = make_tensor(3)
-        b = make_tensor(4)
-        c = broadcasted_add(a, b)
-        d = partial_max(c)
+if __name__ == "__main__":
+    a = make_tensor(3)
+    b = make_tensor(4)
+    c = broadcasted_add(a, b)
+    d = partial_max(c)
 
-        f = function([a, b,], d)
+    f = function([a, b,], d)
 
-        rng = np.random.RandomState([1, 2, 3])
-        a_value = rng.randn(2, 2, 2).astype(a.dtype)
-        b_value = rng.rand(2, 2, 2, 2).astype(b.dtype)
-        c_value = np.transpose(a_value, (2, 1, 0))[:, None, :, :] + b_value
-        expected = c_value.max(axis=1).max(axis=1)
+    rng = np.random.RandomState([1, 2, 3])
+    a_value = rng.randn(2, 2, 2).astype(a.dtype)
+    b_value = rng.rand(2, 2, 2, 2).astype(b.dtype)
+    c_value = np.transpose(a_value, (2, 1, 0))[:, None, :, :] + b_value
+    expected = c_value.max(axis=1).max(axis=1)
 
-        actual = f(a_value, b_value)
+    actual = f(a_value, b_value)
 
-        assert np.allclose(actual, expected), (actual, expected)
-        print "SUCCESS!"
+    assert np.allclose(actual, expected), (actual, expected)
+    print "SUCCESS!"
