4f2fd6322759cae5d424b09b748fbdf813b799b6
David Leonard <sephirothcloud1025@yahoo.com>
2014-07-11 21:08:13 -0400

    Adding solution for traversal.py

--- traverse.py
+++ traverse.py
@@ -1,6 +1,6 @@
 import numpy as np
 from theano import tensor as T
-raise NotImplementedError("Add any imports you need.")
+from theano.gof import Variable
 
 def arg_to_softmax(prob):
     """
@@ -14,7 +14,20 @@ def arg_to_softmax(prob):
     If it is, return the argument to the softmax.
     """
 
-    raise NotImplementedError("Implement this function.")
+    if not isinstance(prob, Variable):
+        raise TypeError()
+
+    if prob.owner is None:
+        raise TypeError()
+
+    owner = prob.owner
+
+    if not isinstance(owner.op, T.nnet.Softmax):
+        raise TypeError()
+
+    rval, = owner.inputs
+
+    return rval
 
 if __name__ == "__main__":
     x = np.ones((5, 4))
